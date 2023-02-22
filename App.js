72a0048ce1521fdd4d12e81e27275c3db0efb7e4
mdumont67 <m.dumont67@hotmail.com>
2023-02-22 16:19:23 +0100

    second commit with color choice

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Compass } from './src/sections/Compass';
+import { Parameter } from './src/sections/Parameter';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { CompassProvider } from './src/provider/CompassProvider'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <CompassProvider>
+            <Stack.Navigator>
+              <Stack.Screen name="Compass" component={Compass} options={{ title: 'Find your own way' }} />
+              <Stack.Screen name="Parameter" component={Parameter} options={{ title: 'Parameter' }} />
+            </Stack.Navigator>
+          </CompassProvider>
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// x:0 ; y: 12 ; z: -18 
